abe2b8537152a15d9bd0aef81340c4ddb5f89ae6
Ananthu TD <ananthutd2021@gmail.com>
2023-08-15 01:08:23 +0530

    initializing application

--- src/app.js
+++ src/app.js
@@ -2,38 +2,78 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import express from 'express';
 import dotenv from 'dotenv';
+import 'dotenv/config';
 import userRouter from './routes/user.js';
 import adminRouter from './routes/admin.js';
-
-dotenv.config({ path: 'env/.env' });
+import { sequelize } from './sequelize/db.js';
+import * as models from './sequelize/models.js';
 
 const filenameUrl = import.meta.url;
 const dirname = path.dirname(fileURLToPath(filenameUrl));
 
+dotenv.config();
+
 const app = express();
 
-app.use(express.static(path.join(dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+async function assertDatabaseConnectionOk() {
+  console.log(`Checking database connection...`);
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection OK!');
+    models.sync();
+  } catch (error) {
+    console.log('Unable to connect to the database:');
+    console.log(error.message);
+    throw new Error(
+      'Unable to connect to the database. Check your database configuration.',
+    );
+  }
+}
 
-// Error handling
-app.use((err, req, res) => {
-  console.error(err);
-  res.status(500).send('An error occurred');
-});
+function setupMiddlewares() {
+  app.use(express.static(path.join(dirname, 'public')));
+  app.use(express.urlencoded({ extended: true }));
+  app.set('view engine', 'ejs');
 
-// Logging
-app.use((req, res, next) => {
-  console.log(`Request: ${req.method} ${req.url}`);
-  next();
-});
+  app.use((err, req, res) => {
+    console.error(err);
+    res.status(500).send('An error occurred');
+  });
 
-// Routes
-app.use('/', userRouter);
-app.use('/admin', adminRouter);
+  app.use((req, res, next) => {
+    const start = Date.now();
+    console.log(
+      `[${new Date().toISOString()}] Request: ${req.method} ${req.url}`,
+    );
+    res.on('finish', () => {
+      const duration = Date.now() - start;
+      console.log(
+        `[${new Date().toISOString()}] Response: ${
+          res.statusCode
+        } - ${duration}ms`,
+      );
+    });
+    next();
+  });
+}
 
-// Start the server
-const port = process.env.PORT;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+function setupRoutes() {
+  app.use('/', userRouter);
+  app.use('/admin', adminRouter);
+}
+
+function startServer() {
+  const port = process.env.PORT;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+
+async function init() {
+  await assertDatabaseConnectionOk();
+  setupMiddlewares();
+  setupRoutes();
+  startServer();
+}
+
+init();
