d6c971983c21ab0101c39abc64ba04768a71f6cf
Ananthu TD <ananthutd2021@gmail.com>
2023-08-15 01:07:11 +0530

    for any extra setup for models

---
+++ src/sequelize/extra-setup.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-unused-vars
+export default function applyExtraSetup(sequelize) {
+  //   const { instrument, orchestra } = sequelize.models;
+  /*   orchestra.hasMany(instrument);
+  instrument.belongsTo(orchestra); */
+}
