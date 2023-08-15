c99751e3177fff6111d561447811b6138a847c9a
Ananthu TD <ananthutd2021@gmail.com>
2023-08-15 10:50:11 +0530

    using a single connection of sequelize for all models

--- src/sequelize/models.js
+++ src/sequelize/models.js
@@ -1,6 +1,7 @@
+import { sequelize } from './db.js';
 import department from './models/department.js';
 
-const models = { Department: department };
+const models = { Department: department(sequelize) };
 
 const sync = () => {
   Object.values(models).forEach((model) => {
