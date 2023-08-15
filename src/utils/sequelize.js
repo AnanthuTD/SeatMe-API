a5148f270894bee052cdd826b1eb04857cab1ebb
Ananthu TD <ananthutd2021@gmail.com>
2023-08-15 10:53:21 +0530

    docemented

--- src/utils/sequelize.js
+++ src/utils/sequelize.js
@@ -1,5 +1,11 @@
 import { Sequelize } from 'sequelize';
 
+/**
+ * Check if an object is an instance of the Sequelize class.
+ *
+ * @param {object} object - The object to check.
+ * @returns {boolean} True if the object is an instance of Sequelize, otherwise false.
+ */
 function isSequelizeInstance(object) {
   return object instanceof Sequelize;
 }
