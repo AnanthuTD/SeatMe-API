bf540dd3aeda10396980969f0040edf0d091a047
Ananthu TD <ananthutd2021@gmail.com>
2023-08-18 23:10:57 +0530

    added authAdminMiddleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,10 +1,13 @@
 import jwt from 'jsonwebtoken';
 
-const secretKey = 'yourSecretKeyHere';
+const secretKey = process.env.SECRET_KEY;
 
 const authMiddleware = (req, res, next) => {
-  const token = req.header('auth-token');
-  if (!token) return res.status(401).send('Access denied.');
+  const { token } = req.cookies;
+  if (!token)
+    return res
+      .status(401)
+      .send('Access denied. You need a valid token to access this route.');
 
   try {
     const verified = jwt.verify(token, secretKey);
@@ -13,12 +16,29 @@ const authMiddleware = (req, res, next) => {
   } catch (error) {
     res.status(400).send('Invalid token.');
   }
+  return null;
 };
 
 const authAdminMiddleware = (req, res, next) => {
-  authAdminMiddleware(req, res, next);
-  if (req.user.admin) next();
-  res.status(200).send('not authorized admin. Visit staff page instead.');
+  const { token } = req.cookies;
+
+  if (!token)
+    return res
+      .status(401)
+      .send('Access denied. You need a valid token to access this route.');
+
+  try {
+    const verified = jwt.verify(token, secretKey);
+    if (!verified.admin)
+      return res
+        .status(200)
+        .send('not authorized admin. Visit staff page instead.');
+    req.admin = verified;
+    next();
+  } catch (error) {
+    res.status(400).send('Invalid token.');
+  }
+  return null;
 };
 
 export { authMiddleware, authAdminMiddleware };
