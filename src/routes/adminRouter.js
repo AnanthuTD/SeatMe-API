6af454565f11a9b6e16f62b4f705f4fd2cdefb9a
Ananthu TD <ananthutd2021@gmail.com>
2023-08-19 14:02:08 +0530

    added route for staff creation

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,9 +1,28 @@
 import express from 'express';
+import { createStaff } from '../utils/bcryptUtils.js';
 
 const router = express.Router();
 
+/**
+ * @route GET /admin
+ * @desc Display the admin page.
+ */
 router.get('/', (req, res) => {
   res.send('admin page');
+});
+
+/**
+ * @route POST /admin/create-staff
+ * @desc Create a new staff member.
+ * @param {object} req.body.staffData - The user data for the new staff member.
+ * @returns {object} Response object with status and message indicating the result of the operation.
+ */
+router.post('/create-staff', async (req, res) => {
+  const { staffData } = req.body;
+
+  const result = await createStaff(staffData);
+
+  return res.status(result.status).json(result);
 });
 
 export default router;
