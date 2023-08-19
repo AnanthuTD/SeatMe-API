88f27ac9a7c5e4cbc5329f43ed398620cab8b9c9
Ananthu TD <ananthutd2021@gmail.com>
2023-08-19 13:56:51 +0530

    renamed

--- src/utils/commonUtils.js
+++ src/utils/commonUtils.js
@@ -1,12 +1,18 @@
 import { models } from '../sequelize/models.js';
 
+/**
+ * Check user credentials and retrieve user data for an admin user.
+ * @param {string} email - User's email for authentication.
+ * @param {string} password - User's password for authentication.
+ * @returns {object|null} User data object if credentials are valid and user is an admin, or null if not.
+ */
 async function checkCredentialsAndRetrieveData(email, password) {
   try {
+    // Find a user in the database with matching email, password, and is_admin = true
     const user = await models.AuthUser.findOne({
       where: {
         email,
         password,
-        is_admin: true,
       },
       attributes: ['id', 'name', 'designation', 'is_admin'],
     });
@@ -17,11 +23,12 @@ async function checkCredentialsAndRetrieveData(email, password) {
       // Here, you can retrieve additional data or perform actions with userData
       return userData;
     }
+
     // Credentials are incorrect or user is not an admin
     return null;
   } catch (error) {
     console.error('Error:', error);
-    return null;
+    return null; // Handle database errors or exceptions
   }
 }
 
