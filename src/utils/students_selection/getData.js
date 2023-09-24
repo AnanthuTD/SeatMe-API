6ad3529a41ad660e8ea5985742a8a6cacedcefd7
Ananthu TD <ananthutd2021@gmail.com>
2023-09-24 19:15:41 +0530

    to fetch exam data from db

--- src/utils/students_selection/getData.js
+++ src/utils/students_selection/getData.js
@@ -1,81 +1,111 @@
 import { Op } from 'sequelize';
-import { models, sequelize } from '../../sequelize/models.js';
+import { models } from '../../sequelize/models.js';
 
-const date = new Date().toISOString().split('T')[0];
-
-let data = await models.DateTime.findAll({
-    where: { date },
-    include: {
-        model: models.Course,
-        nested: true,
-        attributes: ['id', 'name', 'semester'],
-        include: {
-            model: models.Program,
-            attributes: ['id', 'name'],
-            through: {
-                attributes: [],
+// Function to fetch data from the database
+async function fetchData(date) {
+    try {
+        const data = await models.DateTime.findAll({
+            where: { date },
+            include: {
+                model: models.Course,
+                nested: true,
+                attributes: ['id', 'name', 'semester'],
+                include: {
+                    model: models.Program,
+                    attributes: ['id', 'name'],
+                    through: {
+                        attributes: [],
+                    },
+                },
             },
-        },
-    },
-    attributes: [],
-});
-// console.log(JSON.stringify(data, null, 4));
-
-const students = await models.Student.findAll({
-    where: {
-        [Op.or]: data.flatMap((dateTime) =>
-            dateTime.Courses.flatMap((course) =>
-                course.Programs.map((program) => ({
-                    ProgramId: program.id,
-                    semester: course.semester,
-                })),
-            ),
-        ),
-    },
-    attributes: ['name', 'id', 'semester', 'program_id'],
-    raw: true,
-});
-
-students.map((student) => {
-    // Use find to search for the matching program in the data structure
-    data.forEach((dateTime) =>
-        dateTime.Courses.forEach((course) =>
-            course.Programs.forEach((program) => {
-                /* console.log(
-                    JSON.stringify(program, null, 4),
-                    JSON.stringify(student, null, 4),
-                    JSON.stringify(course, null, 4),
-                ); */
-                if (
-                    program.id === student.program_id &&
-                    course.semester === student.semester
-                ) {
-                    student.programName = program.name;
-                    student.courseName = course.name;
-                    student.courseId = course.id;
-                    return student;
-                }
-                return null;
-            }),
-        ),
-    );
-});
-
-console.log(JSON.stringify(students, null, 4));
-
-const groupedStudents = {};
-
-students.forEach((student) => {
-    const { courseId } = student;
-
-    if (!groupedStudents[courseId]) {
-        groupedStudents[courseId] = [];
+            attributes: [],
+        });
+        return data;
+    } catch (error) {
+        throw new Error(`Error fetching data: ${error.message}`);
     }
+}
 
-    groupedStudents[courseId].push(student);
-});
+// Function to fetch students from the database
+async function fetchStudents(data) {
+    try {
+        const students = await models.Student.findAll({
+            where: {
+                [Op.or]: data.flatMap((dateTime) =>
+                    dateTime.Courses.flatMap((course) =>
+                        course.Programs.map((program) => ({
+                            ProgramId: program.id,
+                            semester: course.semester,
+                        })),
+                    ),
+                ),
+            },
+            attributes: ['name', 'id', 'semester', 'program_id'],
+            raw: true,
+        });
+        return students;
+    } catch (error) {
+        throw new Error(`Error fetching students: ${error.message}`);
+    }
+}
 
-// Convert the groupedStudents object to an array of arrays
-const arrayOfArrays = Object.values(groupedStudents);
+// Function to match students with programs and courses
+function matchStudentsWithData(students, data) {
+    students.forEach((student) => {
+        data.forEach((dateTime) =>
+            dateTime.Courses.forEach((course) =>
+                course.Programs.forEach((program) => {
+                    if (
+                        program.id === student.program_id &&
+                        course.semester === student.semester
+                    ) {
+                        student.programName = program.name;
+                        student.courseName = course.name;
+                        student.courseId = course.id;
+                    }
+                }),
+            ),
+        );
+    });
+}
 
-console.log(JSON.stringify(arrayOfArrays, null, 4));
+// Function to group students by courseId
+function groupStudentsByCourseId(students) {
+    const groupedStudents = {};
+
+    students.forEach((student) => {
+        const { courseId } = student;
+
+        if (!groupedStudents[courseId]) {
+            groupedStudents[courseId] = [];
+        }
+
+        groupedStudents[courseId].push(student);
+    });
+
+    return Object.values(groupedStudents);
+}
+
+// Main function to execute the code
+export default async function getData() {
+    const date = new Date().toISOString().split('T')[0];
+
+    try {
+        const data = await fetchData(date);
+        const students = await fetchStudents(data);
+        const totalStudents = students.length;
+
+        matchStudentsWithData(students, data);
+
+        // console.log(JSON.stringify(students, null, 4));
+
+        const exams = groupStudentsByCourseId(students);
+
+        // console.log(JSON.stringify(exams, null, 4));
+
+        return { exams, totalStudents };
+    } catch (error) {
+        console.error(error.message);
+        return null;
+    }
+}
