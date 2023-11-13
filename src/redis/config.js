d859e4b8053d5d0d473569f77481a2a83b5f3e1b
Ananthu TD <ananthutd2021@gmail.com>
2023-11-13 18:31:21 +0530

    added error handling

--- src/redis/config.js
+++ src/redis/config.js
@@ -8,8 +8,23 @@ const redisClient = new Redis({
     host: process.env.REDIS_HOST,
 });
 
+// Event handler for error event
 redisClient.on('error', (err) => {
     console.error(`Redis Error: ${err}`);
 });
+
+// Check if the Redis client is ready after initialization
+redisClient.on('ready', () => {
+    console.log('Connected to Redis server');
+});
+
+// Check if the Redis client encountered a connection issue during initialization
+if (
+    redisClient.status === 'connecting' ||
+    redisClient.status === 'reconnecting'
+) {
+    console.error('Failed to connect to Redis server during initialization');
+    // Handle the failure appropriately, such as exiting the application or taking corrective actions
+}
 
 export default redisClient;
