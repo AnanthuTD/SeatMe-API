b48adba28ef899ffc1649ae8c52905239ba0209d
Ananthu TD <ananthutd2021@gmail.com>
2023-11-14 19:32:06 +0530

    store and retrieve user data from cookies

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,7 +4,6 @@ import {
     getUpcomingExamsFromDB,
     retrieveStudentDetails,
 } from '../helpers/adminHelpers/studentSeat.js';
-import logger from '../helpers/logger.js';
 
 const router = express.Router();
 
@@ -19,6 +18,16 @@ router.get('/', async (req, res) => {
     try {
         const { studentId } = req.query;
 
+        const existingStudentId = req.cookies.studentId;
+        if (existingStudentId && existingStudentId !== studentId) {
+            res.clearCookie('studentId');
+            res.clearCookie('programId');
+            res.clearCookie('semester');
+            res.clearCookie('openCourse');
+        }
+
+        res.cookie('studentId', studentId);
+
         if (!studentId) {
             return res.status(400).json({ error: 'Student ID is required.' });
         }
@@ -29,6 +38,12 @@ router.get('/', async (req, res) => {
             return res.status(404).json({ error: 'Student details not found' });
         }
 
+        const { programId, semester, openCourseId } = seatingInfo;
+
+        res.cookie('programId', programId);
+        res.cookie('semester', semester);
+        res.cookie('openCourseId', openCourseId);
+
         return res.json({ seatingInfo });
     } catch (error) {
         console.error('An error occurred:', error);
@@ -38,7 +53,23 @@ router.get('/', async (req, res) => {
 
 router.get('/exams', async (req, res) => {
     try {
-        const { programId, semester, openCourseId } = req.query;
+        const { programId, semester, openCourseId, studentId } = req.cookies;
+
+        if (!programId || !semester) {
+            if (!studentId)
+                return res.status(400).json({
+                    error: 'No necessary data to process the request!',
+                });
+
+            try {
+                const upcomingExams = await getUpcomingExamsFromDB(studentId);
+
+                return res.status(200).json(upcomingExams);
+            } catch (error) {
+                console.error('An error occurred:', error);
+                return res.status(500).json({ error: 'Internal Server Error' });
+            }
+        }
 
         let upcomingExams = [];
 
@@ -50,28 +81,37 @@ router.get('/exams', async (req, res) => {
             );
         }
 
-        return res
-            .status(upcomingExams.length > 0 ? 200 : 204)
-            .json(upcomingExams);
+        return res.status(200).json(upcomingExams);
     } catch (error) {
         console.error('An error occurred:', error);
-        return res.status(500).json({ error: 'Internal Server Error' });
+
+        const { studentId } = req.cookies;
+        if (!studentId)
+            return res
+                .status(500)
+                .json({ error: 'Failed to load data from redis and db!' });
+        try {
+            const upcomingExams = await getUpcomingExamsFromDB(studentId);
+
+            return res.status(200).json(upcomingExams);
+        } catch (err) {
+            console.error('An error occurred:', err);
+            return res.status(500).json({ err: 'Internal Server Error' });
+        }
     }
 });
 
-router.get('/exams/:studentId', async (req, res) => {
+/* router.get('/exams/:studentId', async (req, res) => {
     try {
         const { studentId } = req.params;
 
         const upcomingExams = await getUpcomingExamsFromDB(studentId);
 
-        return res
-            .status(upcomingExams.length > 0 ? 200 : 204)
-            .json(upcomingExams);
+        return res.status(200).json(upcomingExams);
     } catch (error) {
         console.error('An error occurred:', error);
         return res.status(500).json({ error: 'Internal Server Error' });
     }
-});
+}); */
 
 export default router;
