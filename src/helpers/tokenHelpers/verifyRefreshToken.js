7d820dd5bbe8184b7e589d4402920357e98def93
Ananthu TD <ananthutd2021@gmail.com>
2023-11-16 12:17:23 +0530

    updated

--- src/helpers/tokenHelpers/verifyRefreshToken.js
+++ src/helpers/tokenHelpers/verifyRefreshToken.js
@@ -1,22 +1,33 @@
 import jwt from 'jsonwebtoken';
-import { models } from '../../sequelize/models.js';
+import redisClient from '../../redis/config.js';
+
+const getRefreshTokenFromRedis = async (userId) => {
+    try {
+        // Retrieve the refresh token from Redis using the user ID
+        const refreshToken = await redisClient.get(`refreshToken:${userId}`);
+        return refreshToken;
+    } catch (error) {
+        console.error('Error retrieving refresh token from Redis:', error);
+        throw error; // Handle the error appropriately in your application
+    }
+};
 
 const verifyRefreshToken = async (refreshToken) => {
     const privateKey = process.env.REFRESH_TOKEN_PRIVATE_KEY;
 
     try {
-        const newAccessToken = jwt.verify(refreshToken, privateKey);
+        const decodedToken = jwt.verify(refreshToken, privateKey);
 
-        const refreshTokenRecord = await models.refreshToken.findOne({
-            where: { token: refreshToken },
-        });
+        const refreshTokenRecord = await getRefreshTokenFromRedis(
+            decodedToken.id,
+        );
 
         if (!refreshTokenRecord) {
             throw new Error('Invalid refresh token');
         }
 
         return {
-            tokenDetails: newAccessToken,
+            tokenDetails: decodedToken,
             error: false,
             message: 'Valid refresh token',
         };
