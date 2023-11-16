78503a8158c85362d6bc528c0e591f407fb8b4df
Ananthu TD <ananthutd2021@gmail.com>
2023-11-16 12:16:47 +0530

    added new field expirationTime and new fun getNonExpiredTokens.

--- src/sequelize/models/refreshToken.js
+++ src/sequelize/models/refreshToken.js
@@ -7,15 +7,47 @@ export default (sequelize) => {
     const refreshToken = sequelize.define(
         'refreshToken',
         {
+            authUserId: {
+                type: DataTypes.STRING(7),
+                primaryKey: true,
+            },
             token: {
                 type: DataTypes.TEXT,
+                allowNull: false,
+            },
+            expirationTime: {
+                type: DataTypes.DATE,
                 allowNull: false,
             },
         },
         {
             underscored: true,
+            timestamps: false,
         },
     );
+
+    refreshToken.getNonExpiredTokens = async (authUserId = null) => {
+        // Find and delete expired tokens
+        await refreshToken.destroy({
+            where: {
+                expirationTime: {
+                    [Sequelize.Op.lt]: new Date(),
+                },
+            },
+        });
+
+        // Define the condition based on whether authUserId is provided
+        const condition = authUserId
+            ? { authUserId, expirationTime: { [Sequelize.Op.gte]: new Date() } }
+            : { expirationTime: { [Sequelize.Op.gte]: new Date() } };
+
+        // Retrieve and return non-expired tokens
+        const nonExpiredTokens = await refreshToken.findAll({
+            where: condition,
+        });
+
+        return nonExpiredTokens;
+    };
 
     return refreshToken;
 };
