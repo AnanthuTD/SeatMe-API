0d450adb8c0bb927a44c5f47c85263aca8ac5c8d
Ananthu TD <ananthutd2021@gmail.com>
2023-12-14 21:47:36 +0530

    updated

--- src/sequelize/connection.js
+++ src/sequelize/connection.js
@@ -1,7 +1,13 @@
+import path from 'path';
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import getRootDir from '../../getRootDir.js';
 
-dotenv.config();
+// Specify the path to your .env file
+const envFilePath = path.resolve(getRootDir(), '.env');
+
+// Load environment variables from the specified file
+dotenv.config({ path: envFilePath });
 
 const username = process.env.DB_USER_NAME;
 const password = process.env.DB_PASSWORD;
@@ -16,6 +22,7 @@ const config = {
     dialectOptions: {
         connectTimeout: 30000,
     },
+    // logging: false,
 };
 
 const sequelize = new Sequelize(database, username, password, config);
