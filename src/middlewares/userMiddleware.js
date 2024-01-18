10343c7aed839402cb53f38999f4c6b302aba9b3
Ananthu TD <ananthutd2021@gmail.com>
2024-01-18 22:50:14 +0530

    debugging

--- src/middlewares/userMiddleware.js
+++ src/middlewares/userMiddleware.js
@@ -63,7 +63,8 @@ const checkSeatingAvailability = async (req, res, next) => {
     }
 
     const seatingConfigurations = JSON.parse(seatingConfigList);
-    const currentTime = new dayjs();
+    const currentTime = new dayjs().tz('Asia/Kolkata');
+    console.log('current: ', currentTime);
 
     // Check if the current time is within any of the configured ranges
     const matchingConfig = seatingConfigurations.find((config) => {
