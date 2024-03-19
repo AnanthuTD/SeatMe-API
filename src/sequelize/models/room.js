aea968bca0b22d3511f3217066cdfc7418662fff
AnanthuTD <ananthutd2021@gmail.com>
2024-03-19 21:30:12 +0530

    bug fix

--- src/sequelize/models/room.js
+++ src/sequelize/models/room.js
@@ -43,6 +43,7 @@ export default (sequelize) => {
             priority: {
                 type: DataTypes.TINYINT.UNSIGNED,
                 defaultValue: 1,
+                allowNull: false,
             },
         },
         {
